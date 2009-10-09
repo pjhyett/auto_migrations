29592a2a17d3195308e66bd339fc35b850511fe4
Zhang Yuanyi <yzhang@elctech.com>
2009-10-09 21:56:17 +0800

    add support of primary_key option

--- lib/auto_migrations.rb
+++ lib/auto_migrations.rb
@@ -79,7 +79,8 @@ module AutoMigrations
     
       # Grab schema columns (lifted from active_record/connection_adapters/abstract/schema_statements.rb)
       table_definition = ActiveRecord::ConnectionAdapters::TableDefinition.new(ActiveRecord::Base.connection)
-      table_definition.primary_key(options[:primary_key] || "id") unless options[:id] == false
+      primary_key = options[:primary_key] || "id"
+      table_definition.primary_key(primary_key) unless options[:id] == false
       yield table_definition
       fields_in_schema = table_definition.columns.inject({}) do |hash, column|
         hash[column.name.to_s] = column
@@ -102,7 +103,7 @@ module AutoMigrations
       end
       
       (fields_in_schema.keys & fields_in_db.keys).each do |field|
-        if field != 'id' #ActiveRecord::Base.get_primary_key(table_name)
+        if field != primary_key #ActiveRecord::Base.get_primary_key(table_name)
           changed  = false  # flag
           new_type = fields_in_schema[field].type.to_sym
           new_attr = {}
